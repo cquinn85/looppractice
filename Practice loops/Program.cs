127b4acd85f27a0f65c703e211d3a2e58121cdc5
cquinn85 <cequinn300@gmail.com>
2020-08-26 13:32:25 -0500

    loop practice

---
+++ Practice loops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Practice_loops
+{
+    class Program
+    {
+        private static int x;
+
+        static void Main(string[] args)
+        {
+            
+
+            for (int x = 0; x < 10; x++)
+            {
+                Console.WriteLine(x) ;
+            }
+
+            var myArray = new int[] { 2, 4, 6, 8, 10 };
+
+            foreach (var item in myArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
